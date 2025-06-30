d0d4599bf59ad77c4bd8f63ce52b4266a72d7d78
AADHISESHAN K S <83263942+aadhiseshanks@users.noreply.github.com>
2025-06-30 19:16:56 +0530

    .

---
+++ SORTING/MERGESORT.java
@@ -0,0 +1,57 @@
+import java.util.*;
+class Main {
+    public static void sort(int arr[], int left, int right) {
+        if(left < right) {
+            int mid = (left + right)/2;
+            
+            sort(arr, left, mid);
+            sort(arr, mid + 1, right);
+            
+            merge(arr, left, mid, right);
+        }
+    }
+    
+    public static void merge(int arr[], int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+        
+        for(int i=0; i<n1; i++) 
+            L[i] = arr[left + i];
+        for(int j=0; j<n2; j++) 
+            R[j] = arr[mid + 1 + j];
+        
+        int i = 0, j = 0, k = left;
+        while(i < n1 && j < n2) {
+            if(L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        
+        while(i < n1) 
+            arr[k++] = L[i++];
+        while(j < n2) 
+            arr[k++] = R[j++];
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for(int i=0; i<size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sort(arr, 0, arr.length - 1);
+        for(int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
