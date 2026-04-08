339be694b62f59db949f3a9bd4c3abe6bd7be560
AADHISESHAN K S <83263942+aadhiseshanks@users.noreply.github.com>
2026-04-08 15:20:42 +0530

    Update CLIMBINGSTAIRS.java

--- DYNAMIC PROGRAMMING/CLIMBINGSTAIRS.java
+++ DYNAMIC PROGRAMMING/CLIMBINGSTAIRS.java
@@ -14,3 +14,22 @@ public class Main
 		System.out.print(climbstairs(n));
 	}
 }
+
+// Memoization
+import java.util.Scanner;
+public class Main {
+    public static int climbstairs(int n, int[] dp) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (dp[n] != 0) return dp[n];
+        dp[n] = climbstairs(n-1, dp) + climbstairs(n-2, dp);
+        return dp[n];
+    }
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    int dp[] = new int[n+1];
+		System.out.print(climbstairs(n, dp));
+	}
+}
